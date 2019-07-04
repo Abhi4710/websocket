d857dea46c336351e481ff1857e01d8aa66e7f6a
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-04 16:21:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# websocket
+Hell World!!
---
+++ app.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+
+async def response(websocket, path):
+    message = await websocket.recv()  # receive the message
+    print("Message Received!")
+    print(f"Message: {message}")
+    await websocket.send("Message receive successfully!!")  # send the message back
+
+start_server = websockets.serve(response, 'localhost', 1234) ## (response, host, port)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
