8e5e5c6d5cade8b92c2faf019d6112b510bfbc70
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-04 17:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ async def response(websocket, path):
     print("Message Received!")
     print(f"Message: {message}")
     await websocket.send("Message receive successfully!!")  # send the message back
-port = int(os.environ.get("PORT", 5000)) 
+port = int(process.env.PORT || 1234) 
 start_server = websockets.serve(response, 'localhost', port=port) ## (response, host, port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
