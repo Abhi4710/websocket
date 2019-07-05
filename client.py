2392f9e9e06ea1f40a2fbdfcc4260fd0e644fd2c
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-05 11:37:09 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import asyncio
 import websockets
 import os
+import codecs
 
 
 async def message():
     port = int(os.environ['PORT'])
+    f = codecs.open("webclient.html", 'r', 'utf-8')
     async with websockets.connect(f"wss://abhi122.herokuapp.com:{port}") as socket:
         msg = "Hello!!"
         await socket.send(msg)
